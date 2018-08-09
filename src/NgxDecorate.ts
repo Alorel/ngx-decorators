f621a4a39823d3abdf9364da5547df86a3e15aca
Arturas <a.molcanovas@gmail.com>
2018-08-10 00:44:57 +0100

    refactor(NgxDecorate): Reduce nesting level

--- src/NgxDecorate.ts
+++ src/NgxDecorate.ts
@@ -1,38 +1,43 @@
 import {MockDestroyable, MockInitable} from './lib/Mocks';
+import {noop} from './lib/noop';
 import {complete} from './processors/complete';
 import {destroyed} from './processors/destroyed';
 import {lazySubj} from './processors/lazy-subject';
 import {setProp} from './processors/setProp';
 import {unsubscribe} from './processors/unsubscribe';
 
+function applyOnInit(proto: MockInitable): void {
+  const orig = proto.ngOnInit || noop;
+
+  proto.ngOnInit = function ngOnInit(): void {
+    try {
+      orig.apply(this, arguments);
+    } finally {
+      setProp(this);
+    }
+  };
+}
+
+function applyOnDestroy(proto: MockDestroyable): void {
+  const orig = proto.ngOnDestroy || noop;
+
+  proto.ngOnDestroy = function ngOnDestroy(): void {
+    try {
+      orig.apply(this, arguments);
+    } finally {
+      destroyed(this);
+      unsubscribe(this);
+      complete(this);
+      lazySubj(this);
+    }
+  };
+}
+
 /** Apply all the decorators that tap into Angular's lifecycle hooks */
 export function NgxDecorate(): ClassDecorator {
   return (target: any): void => {
     const proto: MockDestroyable & MockInitable = target.prototype;
-    const destr = proto.ngOnDestroy;
-    const init = proto.ngOnInit;
-
-    proto.ngOnInit = function ngOnInit(): void {
-      try {
-        if (init) {
-          init.apply(this, arguments);
-        }
-      } finally {
-        setProp(this);
-      }
-    };
-
-    proto.ngOnDestroy = function ngOnDestroy(): void {
-      try {
-        if (destr) {
-          destr.apply(this, arguments);
-        }
-      } finally {
-        destroyed(this);
-        unsubscribe(this);
-        complete(this);
-        lazySubj(this);
-      }
-    };
+    applyOnInit(proto);
+    applyOnDestroy(proto);
   };
 }
