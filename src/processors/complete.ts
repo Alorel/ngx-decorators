c6ae4b4446f3018f78ed3635ab96f6b50e5ee943
Arturas <a.molcanovas@gmail.com>
2018-08-10 00:48:18 +0100

    refactor(Complete): Reduce code desting level

--- src/processors/complete.ts
+++ src/processors/complete.ts
@@ -2,17 +2,21 @@ import {completeArray} from '../lib/completeArray';
 import {MockCompletable} from '../lib/Mocks';
 import {_complete} from '../lib/symbols';
 
+function processProp(prop: MockCompletable | MockCompletable[]): void {
+  if (prop) {
+    if (Array.isArray(prop)) {
+      completeArray(prop);
+    } else {
+      prop.complete();
+    }
+  }
+}
+
 /** @internal */
 export function complete(self: any): void {
   if (self[_complete]) {
     for (const prop of self[_complete]) {
-      if (self[prop]) {
-        if (Array.isArray(self[prop])) {
-          completeArray(self[prop]);
-        } else {
-          (<MockCompletable>self[prop]).complete();
-        }
-      }
+      processProp(self[prop]);
     }
   }
 }
