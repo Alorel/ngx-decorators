052b18c67718c33579baabdc2df266f9cb5b2bdb
Arturas <a.molcanovas@gmail.com>
2018-08-14 13:59:16 +0100

    test(package): Generate tests asynchronously

--- test/Package.ts
+++ test/Package.ts
@@ -1,44 +1,91 @@
+import * as Bluebird from 'bluebird';
 import {expect} from 'chai';
 import * as spawn from 'cross-spawn';
 import * as fs from 'fs-extra';
 import * as glob from 'glob';
+import {Test} from 'mocha';
 import * as path from 'path';
 import * as rmrf from 'rimraf';
 
 describe('Package', function() {
-  this.timeout(120000);
-
   interface File {
     contents: string;
     path: string;
   }
 
-  const cwd: string = path.dirname(require.resolve('../package.json'));
+  let cwd: string;
+  let paths: string[];
+  let files: File[];
 
-  spawn.sync('npm', ['run', 'build'], {
-    cwd,
-    env: Object.assign({}, process.env, {
-      FROM_GENERAL_SUITE: '1'
-    })
+  before('Init cwd', () => {
+    cwd = path.dirname(require.resolve('../package.json'));
   });
 
-  const paths: string[] = glob.sync('dist/**/*.{ts,js}', {cwd, absolute: true});
-  const files: File[] = paths.map((p: string): File => {
-    return {
-      contents: fs.readFileSync(p, 'utf8'),
-      path: path.relative(cwd, p)
-    };
-  });
-
-  it('Should have files in dist', () => {
-    expect(files).to.not.be.empty;
-  });
-
-  for (const f of files) {
-    it(`${f.path} should not include "tslib"`, () => {
-      expect(f.contents).to.not.contain('tslib', `${f.path} contains tslib`);
+  before('Build', (cb: any) => {
+    const proc = spawn('npm', ['run', 'build'], {
+      cwd,
+      env: Object.assign({}, process.env, {
+        FROM_GENERAL_SUITE: '1'
+      })
     });
-  }
+    let errored = false;
+    proc.once('error', (e: any) => {
+        errored = true;
+        cb(e);
+      })
+      .once('exit', (code: number) => {
+        if (errored) {
+          return;
+        }
+        if (code === 0) {
+          cb();
+        } else {
+          cb(new Error(`Code ${code}`));
+        }
+      });
+  });
+
+  before('get paths', (cb: any) => {
+    glob('dist/**/*.{ts,js}', {cwd, absolute: true}, (e: any, p: string[]) => {
+      if (e) {
+        cb(e);
+      } else {
+        paths = p;
+        cb();
+      }
+    });
+  });
+
+  before('Get files', () => {
+    return Bluebird
+      .map(paths, (p: string): Promise<File> => {
+        return fs.readFile(p, 'utf8')
+          .then((contents: string): File => {
+            return {
+              contents,
+              path: path.relative(cwd, p)
+            };
+          });
+      })
+      .then((f: File[]) => {
+        files = f;
+      });
+  });
+
+  before('Create test cases', () => {
+    this.addTest(new Test('Should have files in dist', () => {
+      expect(files).to.not.be.empty;
+    }));
+    for (const f of files) {
+      this.addTest(new Test(`${f.path} should not include "tslib"`, () => {
+        expect(f.contents).to.not.contain('tslib', `${f.path} contains tslib`);
+      }));
+    }
+  });
+
+  it('', () => {
+    //noop
+  });
 
   after('cleanup', (cb: any) => {
     rmrf(path.join(cwd, 'dist'), cb);
