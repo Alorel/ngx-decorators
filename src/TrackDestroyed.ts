62b04afc4f1d3947445808967ccb40302f25ce77
Arturas <a.molcanovas@gmail.com>
2018-08-14 13:28:14 +0100

    fix(duplication): Removed duplicated code in TrackDestroyed and TrackInit

--- src/TrackDestroyed.ts
+++ src/TrackDestroyed.ts
@@ -1,13 +1,9 @@
+import {defineImmutable} from './lib/defineImmutable';
 import {_destroyed} from './lib/symbols';
 
 /** Set the given property to true when the component is destroyed */
 export function TrackDestroyed(): PropertyDecorator {
   return (target: any, value: PropertyKey): void => {
-    Object.defineProperty(target, _destroyed, {
-      configurable: false,
-      enumerable: false,
-      value,
-      writable: false
-    });
+    defineImmutable(target, _destroyed, value);
   };
 }
--- src/TrackInit.ts
+++ src/TrackInit.ts
@@ -1,13 +1,9 @@
+import {defineImmutable} from './lib/defineImmutable';
 import {_init} from './lib/symbols';
 
 /** Set the given property to true when the component is initialised */
 export function TrackInit(): PropertyDecorator {
   return (target: any, value: PropertyKey): void => {
-    Object.defineProperty(target, _init, {
-      configurable: false,
-      enumerable: false,
-      value,
-      writable: false
-    });
+    defineImmutable(target, _init, value);
   };
 }
